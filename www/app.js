af81bb44a1d2120c560bc28be1b9569cf5a41820
ArnaudWierel <66359372+ArnaudWierel@users.noreply.github.com>
2021-09-27 18:02:38 +0200

    Add files via upload

--- www/app.js
+++ www/app.js
@@ -34,12 +34,12 @@ taskformElement.addEventListener('submit', async (e) => {
 
 async function getTasks() {
     const response = await fetch('/api/tasks');
-    const tasks = await response.json();
-    console.log(tasks);
+    const payload = await response.json();
+    console.log(payload);
 
-    if (tasks) {
+    if (payload.response.tasks) {
         document.querySelector("#empty").remove();
-        for (const task of tasks) {
+        for (const task of payload.response.tasks) {
             taskListElement.appendChild(generateTask(task))
         }
     }
